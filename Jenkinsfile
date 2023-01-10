d6e0f412a4b697fe33cab34b1a0754cbe11d176e
Alexandros Stergiakis <a.stergiakis@mwam.com>
2023-01-10 10:22:04 +0000

    CI scripts

---
+++ Jenkinsfile
@@ -0,0 +1,62 @@
+import com.mwam.jenkins.build.NpmBuild
+import com.mwam.jenkins.helpers.*
+import com.mwam.jenkins.ioc.ContextRegistry
+
+ContextRegistry.registerScript(this)
+
+def artifactoryPath = "/artifactory/generic-corelib-local/MWScreenshotSharing/"
+publishBranches = ["master"]
+
+library_init()
+GitVersion.setShouldPublish(publishBranches)
+
+def npm = new NpmBuild(artifactoryPath)
+def agent = new BuildAgent()
+    .addGitversionContainer()
+    .addContainers(npm.getBuildContainer())
+
+agent._podSpec["spec"]["volumes"].add([
+    "name": "chrome-plugin-private-key",
+    "secret": ["secretName": "chrome-plugin-private-key"]
+])
+
+agent._podSpec["spec"]["containers"].find { c -> c.name == "npm" }["volumeMounts"] = [[
+    "mountPath": "/tmp/plugin/",
+    "name": "chrome-plugin-private-key"
+]]
+
+agent.RunInAgent {
+  Script steps = ContextRegistry.getScript()
+
+  steps.stage("setup") {
+    steps.container("gitversion") {
+      steps.sh "gitversion > gitversion.yaml"
+    }
+
+    steps.container("npm") {
+      steps.sh "npm install"
+    }
+  }
+
+  steps.stage("build") {
+    steps.container("npm") {
+      steps.sh "npm run build"
+    }
+  }
+ 
+  if (Publish.instance.shouldPublish) {
+    steps.stage('publish') {
+      steps.container('npm') {
+        steps.withCredentials([steps.usernamePassword(
+              credentialsId: this.OsEnv.getJenkinsSecret('Artifactory'),
+              usernameVariable: "artUser",
+              passwordVariable: "artPass")]) {
+          steps.sh(label: 'Publish - push', script: """
+            cd buildArtifacts && find . -name *.crx -type f -exec \
+            curl -X PUT -H "X-Requested-With: XMLHttpRequest" -u ${steps.artUser}:${steps.artPass} -T {} "https://artifactory.mwam.local/artifactory/generic-corelib-local/MWScreenshotSharing/{}" \;
+          """)
+        }
+      }
+    }
+  }
+}
---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# get MajorMinorPatch from gitversion.json 
+MajorMinorPatch=`node -pe 'JSON.parse(process.argv[1]).MajorMinorPatch' "$(cat gitversion.json)"`
+SemVer=`node -pe 'JSON.parse(process.argv[1]).SemVer' "$(cat gitversion.json)"`
+CrxFile=MWScreenshotSharing-v${SemVer}.crx
+CrxKey=/tmp/plugin/crxkey
+SrcFolder=src
+ExtensionId=jiiphahkgkejfheajligilpmgijkchba
+ArtifactsFolder=buildArtifacts
+
+echo "SDDT build.sh:"
+echo "MajorMinorPatch=${MajorMinorPatch}"
+echo "SemVer=${SemVer}"
+echo "CrxFile=${CrxFile}"
+echo "CrxKey=${CrxKey}"
+echo "SrcFolder=${SrcFolder}"
+echo "ExtensionId=${ExtensionId}"
+echo "ArtifactsFolder=${ArtifactsFolder}"
+
+echo "working directory:"
+pwd
+
+echo "secrets in /tmp/MWScreenshotSharing:"
+ls -la /tmp/plugin
+
+if [ ! -f "$CrxKey" ]; then
+    echo "${CrxKey} doesn't exist, cannot build crx file."
+    exit 1
+fi
+
+if [ ! -r "$CrxKey" ]; then
+    echo "${CrxKey} exist but it's not readable."
+    exit 2
+fi
+
+echo create a symlink from the secret to local key.pem file so build will pick it up
+ln -s ${CrxKey} $(pwd)/key.pem
+
+echo "set version field in package.json"
+npm version ${MajorMinorPatch} --no-git-tag-version --allow-same-version
+
+echo "build plugin"
+sed -i "s/{MajorMinorPatch}/${MajorMinorPatch}/g" src/manifest.json
+node node_modules/crx3/bin/crx3 --key ${CrxKey} --crx ${CrxFile} ${SrcFolder}
+
+echo "prepare manifest.xml:"
+mkdir --verbose -p ${ArtifactsFolder}
+cp --verbose manifest.xml ${ArtifactsFolder}
+sed -i "s/{ExtensionId}/${ExtensionId}/g" ${ArtifactsFolder}/manifest.xml
+sed -i "s/{CrxFile}/${CrxFile}/g" ${ArtifactsFolder}/manifest.xml
+sed -i "s/{MajorMinorPatch}/${MajorMinorPatch}/g" ${ArtifactsFolder}/manifest.xml
+
+echo "moving build artifacts to ${ArtifactsFolder}"
+mkdir --verbose -p ${ArtifactsFolder}
+mv --verbose ${CrxFile} ${ArtifactsFolder}
+
+echo "build.sh: finished"
